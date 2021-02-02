e1148cca062ab1e4b31b542a17cc130a48f783cc
khairnsaa <53510222+khairnsaa@users.noreply.github.com>
2021-02-02 20:35:47 +0700

    Delete js directory

--- js/animations/AnimationEnter.js
+++
@@ -1,6 +0,0 @@
-import { gsap } from "gsap";
-function AnimationEnter(container) {
-    return gsap.from(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none' });
-}
-
-export default AnimationEnter
--- js/animations/AnimationLeave.js
+++
@@ -1,7 +0,0 @@
-import { gsap } from "gsap";
-
-function AnimationLeave(container) {
-    return gsap.to(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none' });
-}
-
-export default AnimationLeave;
